ca010fe5c17d3e7df0b5e359487327fb06040b08
Andrew Maloch <admaloch91@gmail.com>
2023-12-31 08:05:32 -0500

    added gamedata slice

--- src/store/game-data/GameDataSlice.tsx
+++ src/store/game-data/GameDataSlice.tsx
@@ -1,4 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+    roundsPlayed: 0,
+    isGameActive: false,
+}
+
+const deckSlice = createSlice({
+  name: 'game-data',
+  initialState,
+  reducers: {
+    
+  },
+})
+
+export const { updateDeck } = deckSlice.actions
+
+
+export default deckSlice.reducer
 
 
 
