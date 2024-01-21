27be4d1a76963b7c9736d766c48551c2efae8f46
Andrew Maloch <admaloch91@gmail.com>
2024-01-21 11:45:47 -0500

    modal refactor complete

--- src/components/main-game/play-game/end-of-turn/EndOfTurnResults.tsx
+++ src/components/main-game/play-game/end-of-turn/EndOfTurnResults.tsx
@@ -1,10 +1,7 @@
-import Modal from "../../../UI/modal/Modal";
-import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from 'react';
 import { RootState } from "../../../../store/store";
-import ResultsModalContents from "./ResultsModalContent";
-import { updatePlayer } from "../../../../store/player-arr/playersArrSlice";
-
+import ResultsModal from "./ResultsModal";
+import { useSelector } from 'react-redux';
 
 interface EndOfTurnResultsProps {
     playerIndex: number;
@@ -16,50 +13,26 @@ interface EndOfTurnResultsProps {
 
 export default function EndOfTurnResults({ playerIndex, isPlayerFinished, endRound, makePlayerFinished, changeToNextPlayer }: EndOfTurnResultsProps) {
 
-const dispatch = useDispatch()
     const playersArr = useSelector((state: RootState) => state.playersArr);
     const currPlayer = playersArr[playerIndex]
-    const { hand, splitHand, isPlayerSplit } = currPlayer
+    const { hand, isDoubleDown } = currPlayer
 
     useEffect(() => {
         if (hand.cardSum > 21
             || hand.cards.length === 2 && hand.cardSum === 21
-            || hand.cards.length === 3 && currPlayer.isDoubleDown) {
+            || hand.cards.length === 3 && isDoubleDown) {
             setTimeout(() => {
                 endRound()
             }, 1000)
         }
-    }, [playersArr, playerIndex])
-
-    const endResultsBtnHandler = async () => {
-        if (splitHand.cards.length === 1 || playersArr.length - 1 !== playerIndex) {
-            if (!isPlayerSplit || isPlayerSplit && splitHand.cards.length > 1) {
-                changeToNextPlayer()
-            }
-            if (isPlayerSplit) handleSplitRoundResults()
-            makePlayerFinished()
-
-        } else {
-            console.log('begin dealer round')
-        }
-    };
-
-    const handleSplitRoundResults = () => {
-        const updateMainHand = { ...hand }
-        const updateSplitHand = { ...splitHand }
-        dispatch(updatePlayer({ ...playersArr[playerIndex], hand: updateSplitHand, splitHand: updateMainHand, isDoubleDown: false }));
-    }
+    }, [hand, isDoubleDown, endRound])
 
     return (
-        
-        <Modal
-            closeModal={endResultsBtnHandler}
-            open={isPlayerFinished}
-        >
-            <ResultsModalContents
-                playerIndex={playerIndex}
-                endResultsBtnHandler={endResultsBtnHandler}
-            />
-        </Modal>
+        <ResultsModal
+            playerIndex={playerIndex}
+            isPlayerFinished={isPlayerFinished}
+            makePlayerFinished={makePlayerFinished}
+            changeToNextPlayer={changeToNextPlayer}
+        />
     )
 }
---
+++ src/components/main-game/play-game/end-of-turn/ResultsModal.tsx
@@ -0,0 +1,51 @@
+import Modal from "../../../UI/modal/Modal";
+import { useDispatch, useSelector } from "react-redux"
+import { RootState } from "../../../../store/store";
+import ResultsModalContents from "./ResultsModalContent";
+import { updatePlayer } from "../../../../store/player-arr/playersArrSlice";
+
+interface EndOfTurnResultsProps {
+    playerIndex: number;
+    isPlayerFinished: boolean;
+    makePlayerFinished: () => void;
+    changeToNextPlayer: () => void;
+}
+
+export default function ResultsModal({ playerIndex, isPlayerFinished, makePlayerFinished, changeToNextPlayer }: EndOfTurnResultsProps) {
+
+    const dispatch = useDispatch()
+    const playersArr = useSelector((state: RootState) => state.playersArr);
+    const currPlayer = playersArr[playerIndex]
+    const { hand, splitHand, isPlayerSplit } = currPlayer
+
+    const endResultsBtnHandler = async () => {
+        if (splitHand.cards.length === 1 || playersArr.length - 1 !== playerIndex) {
+            if (!isPlayerSplit || isPlayerSplit && splitHand.cards.length > 1) {
+                changeToNextPlayer()
+            }
+            if (isPlayerSplit) handleSplitRoundResults()
+            makePlayerFinished()
+        } else {
+            console.log('begin dealer round')
+        }
+    };
+
+    const handleSplitRoundResults = () => {
+        const updateMainHand = { ...hand }
+        const updateSplitHand = { ...splitHand }
+        dispatch(updatePlayer({ ...playersArr[playerIndex], hand: updateSplitHand, splitHand: updateMainHand, isDoubleDown: false }));
+    }
+
+    return (
+
+        <Modal
+            closeModal={endResultsBtnHandler}
+            open={isPlayerFinished}
+        >
+            <ResultsModalContents
+                playerIndex={playerIndex}
+                endResultsBtnHandler={endResultsBtnHandler}
+            />
+        </Modal>
+    )
+}
