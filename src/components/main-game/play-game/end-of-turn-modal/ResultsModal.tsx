84e3fd91f8051dd229a9117479afd0bb0b96d3fd
Andrew Maloch <admaloch91@gmail.com>
2024-02-20 07:31:13 -0500

    sorted out split hand issue

--- src/components/main-game/play-game/end-of-turn-modal/ResultsModal.tsx
+++ src/components/main-game/play-game/end-of-turn-modal/ResultsModal.tsx
@@ -2,7 +2,7 @@ import Modal from "../../../UI/modal/Modal";
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../../../../store/store";
 import ResultsModalContents from "./ResultsModalContent";
-import { updatePlayer } from "../../../../store/player-arr/playersArrSlice";
+import { updateAllPlayers, updatePlayer } from "../../../../store/player-arr/playersArrSlice";
 import { beginDealerRound, revealDealerCard } from "../../../../store/game-data/GameDataSlice";
 import { delay } from "../../../../utils/Utility";
 import { useEffect } from "react";
@@ -19,6 +19,7 @@ export default function ResultsModal({ playerIndex, isCurrPlayerFinished, makeCu
     const dispatch = useDispatch()
     const playersArr = useSelector((state: RootState) => state.playersArr);
     const currPlayer = playersArr[playerIndex]
+    const playersHaveSplit = playersArr.some(player => player.splitHand.cards.length > 0)
     const { hand, splitHand, isPlayerSplit } = currPlayer
 
     const endResultsBtnHandler = async () => {
@@ -28,23 +29,35 @@ export default function ResultsModal({ playerIndex, isCurrPlayerFinished, makeCu
         } if (splitHand.cards.length === 1 || playersArr.length - 1 !== playerIndex) {
             if (!isPlayerSplit || isPlayerSplit && splitHand.cards.length > 1) {
                 reversePlayerHands()
-                
+
                 changeToNextPlayer()
             }
             makeCurrPlayerNotFinished()
         }
         else {
+            if (playersHaveSplit) {
+                const finalSplitHandUpdate = playersArr.map(x => {
+                    if (x.splitHand.cards.length > 0) {
+                        return {
+                            ...x,
+                            hand: x.splitHand,
+                            splitHand: x.hand,
+                        }
+                    } else return x
+                })
+                dispatch(updateAllPlayers(finalSplitHandUpdate))
+            }
             dispatch(beginDealerRound())
         }
     };
 
     const reversePlayerHands = () => {
-        const updatedSplitHand = { ...hand }
-        const updatedMainHand = { ...splitHand }
+        // const updatedSplitHand = { ...hand }
+        // const updatedMainHand = { ...splitHand }
         dispatch(updatePlayer({
             ...playersArr[playerIndex],
-            hand: updatedMainHand,
-            splitHand: updatedSplitHand,
+            hand: splitHand,
+            splitHand: hand,
             isDoubleDown: false
         }));
     }
