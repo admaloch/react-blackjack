9d1519b3e076c1871059fb09d202d8ab922f4503
Andrew Maloch <admaloch91@gmail.com>
2024-03-04 07:05:14 -0500

    tweaked player stats end turn icon

--- src/components/stats-modal/PlayerStatsItem.tsx
+++ src/components/stats-modal/PlayerStatsItem.tsx
@@ -1,5 +1,7 @@
+import { useSelector } from "react-redux";
 import { PlayerInterface } from "../../models/PlayerProps"
 import ExitTableIcon from "../main-game/play-game/exit-table-modal/ExitTableIcon";
+import { RootState } from "../../store/store";
 
 interface PlayerStatsItemProps {
     player: PlayerInterface;
@@ -7,15 +9,19 @@ interface PlayerStatsItemProps {
 }
 
 export default function PlayerStatsItem({ player, closeStatsModal }: PlayerStatsItemProps) {
-
+    const gameData = useSelector((state: RootState) => state.gameData);
+    const { isBetRoundActive } = gameData
     const { name, bank, roundsWon } = player
 
     return (
         <li>
-            <ExitTableIcon
-             currPlayer={player} 
-             closeStatsModal={closeStatsModal}
-             />
+            {!isBetRoundActive &&
+                <ExitTableIcon
+                    currPlayer={player}
+                    closeStatsModal={closeStatsModal}
+                />
+            }
+
             {name}: Current Bank: {`$${bank}`}: Total Rounds Won: {roundsWon}
         </li>
     )
