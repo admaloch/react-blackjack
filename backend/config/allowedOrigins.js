666cd4d741fc823bc71f3f0321545288b434776a
Davis Maloch <admaloch91@gmail.com>
2024-10-25 11:04:53 -0400

    forgot to update baseUrl in store

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const origin = process.env.NODE_ENV === 'production' 
-    ? ['https://blackjack-react.netlify.app/']
+    ? ['https://blackjack-react.netlify.app']
     : ['http://localhost:5173', 'http://localhost:3500'];
 
 const allowedOrigins = origin;
--- client/src/store/api/gameSessionsApiSlice.ts
+++ client/src/store/api/gameSessionsApiSlice.ts
@@ -1,36 +1,39 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-const nodeEnvironment = import.meta.env.VITE_NODE_ENV
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const nodeEnvironment = import.meta.env.VITE_NODE_ENV;
 
-const url = nodeEnvironment === 'development' ? 'http://localhost:3500' : 'https://blackjack.onrender.com'
+const url =
+  nodeEnvironment === "development"
+    ? "http://localhost:3500"
+    : "https://blackjack-backend-goet.onrender.com/";
 
 const apiSlice = createApi({
-  reducerPath: 'api',
+  reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: url }), // Adjust base URL as needed
   endpoints: (builder) => ({
     getAllGameSessions: builder.query({
-      query: () => 'gameSessions',
+      query: () => "gameSessions",
     }),
     getGameSessionById: builder.query({
       query: (id) => `gameSessions/${id}`,
     }),
     createGameSession: builder.mutation({
       query: (gameSessionData) => ({
-        url: 'gameSessions',
-        method: 'POST',
+        url: "gameSessions",
+        method: "POST",
         body: gameSessionData,
       }),
     }),
     updateGameSession: builder.mutation({
       query: ({ id, ...gameSessionData }) => ({
         url: `gameSessions/${id}`,
-        method: 'PATCH',
+        method: "PATCH",
         body: gameSessionData,
       }),
     }),
     deleteGameSession: builder.mutation({
       query: (id) => ({
         url: `gameSessions/${id}`,
-        method: 'DELETE',
+        method: "DELETE",
       }),
     }),
   }),
